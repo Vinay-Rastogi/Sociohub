30e05f2fbebbe68244df4337f11d2dd87a177dbb
Vinay Rastogi <mvinayrastogi@gmail.com>
2024-12-14 12:49:03 +0530

    Changed Base Url

--- client/src/api.js
+++ client/src/api.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://sociohub-7005.onrender.com"; 
-export const BASE_URL = 'http://localhost:3001'; 
+export const BASE_URL = "https://sociohub-7005.onrender.com"; 
+// export const BASE_URL = 'http://localhost:3001'; 
